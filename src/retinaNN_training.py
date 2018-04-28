987238268aa59456c05e7a0697b978740e90fd8b
unknown <jbrown97@mgh.harvard.edu>
2018-04-28 19:35:27 -0400

    Updated to TF indices - training and inference works

--- src/retinaNN_training.py
+++ src/retinaNN_training.py
@@ -24,6 +24,17 @@ import sys
 from ..lib.help_functions import *
 from ..lib.extract_patches import get_data_training
 
+def dice_coef(y_true, y_pred, smooth=1.):
+
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+
+    return 1 - dice_coef(y_true, y_pred)
 
 
 #Define the neural network
